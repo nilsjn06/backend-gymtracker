d856156ec65dcfc9e8d276f793fda9b51ae01ed9
DarioScholze03 <Dario.Scholze@Student.HTW-Berlin.de>
2025-10-15 15:36:00 +0200

    SpringBoot hinzugefügt

---
+++ src/test/java/htw_berlin/webtech/WebtechApplicationTests.java
@@ -0,0 +1,13 @@
+package htw_berlin.webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
