cc597fd6f7df6fc80a73dd8555ff8d5ba2e20ba0
Nils Neunaß <nils.neunass@gmx.de>
2025-10-17 10:11:29 +0200

    Ordnerstruktur

--- src/main/java/htw_berlin/webtech/rest/WebtechApplication.java
+++ src/main/java/htw_berlin/webtech/rest/WebtechApplication.java
@@ -1,4 +1,4 @@
-package htw_berlin.webtech;
+package htw_berlin.webtech.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
