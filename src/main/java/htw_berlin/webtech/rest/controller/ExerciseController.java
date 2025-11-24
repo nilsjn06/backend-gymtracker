8fdb427a190359d5e263525b63e45cebbf63d12c
DarioScholze03 <Dario.Scholze@Student.HTW-Berlin.de>
2025-11-24 17:34:05 +0100

    verbesserung

--- src/main/java/htw_berlin/webtech/rest/controller/ExerciseController.java
+++ src/main/java/htw_berlin/webtech/rest/controller/ExerciseController.java
@@ -3,19 +3,26 @@ package htw_berlin.webtech.rest.controller;
 import htw_berlin.webtech.rest.model.Exercise;
 import htw_berlin.webtech.rest.service.ExerciseService;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-@CrossOrigin(origins = "https://frontend-gymtrakcer.onrender.com")
+
+@CrossOrigin(origins = "https://frontend-gymtracker.onrender.com")
 @RestController
 public class ExerciseController {
 
     private final ExerciseService service;
 
-    public ExerciseController(ExerciseService service) {
-        this.service = service;
+    public ExerciseController(ExerciseService ExerciseService) {
+        this.service = ExerciseService;
     }
 
     @GetMapping("/exercises")
     public List<Exercise> getAllExercises() {
         return service.getAll();
+    }
+
+    @PostMapping("/exercises")
+    public Exercise createExercise(@RequestBody Exercise exercise) {
+        return service.save(exercise);
     }
 }
--- src/main/java/htw_berlin/webtech/rest/service/ExerciseService.java
+++ src/main/java/htw_berlin/webtech/rest/service/ExerciseService.java
@@ -5,16 +5,47 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ExerciseService {
     private final List<Exercise> exercises = new ArrayList<>();
+
+    public ExerciseService() {
+        exercises.add(new Exercise(
+                UUID.randomUUID().toString(),
+                "Bankdrücken",
+                "Brust",
+                3,
+                10,
+                60.0
+        ));
+        exercises.add(new Exercise(
+                UUID.randomUUID().toString(),
+                "Kniebeuge",
+                "Beine",
+                4,
+                8,
+                80.0
+        ));
+        exercises.add(new Exercise(
+                UUID.randomUUID().toString(),
+                "Klimmzüge",
+                "Rücken",
+                3,
+                12,
+                0.0
+        ));
+    }
 
     public List<Exercise> getAll() {
         return exercises;
     }
 
     public Exercise save(Exercise ex) {
+        if (ex.getId() == null || ex.getId().isBlank()) {
+            ex.setId(UUID.randomUUID().toString());
+        }
         exercises.add(ex);
         return ex;
     }
