b320c74d5ff5db56225bc99320fc0d825677ca13
nilsjn06 <Nils.Neunass@Student.HTW-Berlin.de>
2025-11-25 12:21:07 +0100

    alles neu

--- src/main/java/htw_berlin/webtech/rest/controller/ExerciserController.java
+++ src/main/java/htw_berlin/webtech/rest/controller/ExerciserController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ExcerciserController {
+public class ExerciserController {
 
     @GetMapping(path = "/exercise")
     public ResponseEntity<Exercise> getExercise() {
--- src/main/java/htw_berlin/webtech/rest/model/Exercise.java
+++ src/main/java/htw_berlin/webtech/rest/model/Exercise.java
@@ -2,19 +2,17 @@ package htw_berlin.webtech.rest.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class Exercise {
 
-    private String id;              // ← hinzugefügt
     private String name;
-    private String muskelgruppe;
+    private Muskelgruppe muskelgruppe;
     private int satz;
     private int wiederholungen;
     private double gewicht;
+
 }
