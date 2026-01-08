160b878ac6dc53d5b213543bbd2ece364c853ba1
DarioScholze03 <Dario.Scholze@Student.HTW-Berlin.de>
2026-01-08 15:41:07 +0100

    CI hinzugefügt

--- src/test/java/htw_berlin/webtech/WebtechApplicationTests.java
+++ src/test/java/htw_berlin/webtech/WebtechApplicationTests.java
@@ -3,7 +3,7 @@ package htw_berlin.webtech;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled("Deaktiviert, weil wir keinen Test brauchen")
+
 class WebtechApplicationTests {
 
 	@Test
